28da59721614ebb56b48b9d7786fc9ea9dac2369
xfp23 <120090946+xfp23@users.noreply.github.com>
2023-11-26 14:27:52 +0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,15 +5,16 @@
 #include "img1.h"
 #include "img2.h"
 
+#define unsigned int uint
 
 #define WIDTH 160
 #define  HIGHG 128
 //定义中断引脚
-const int interrupt_monday=13;
-const int interrupt_tuesday=12;
-const int interrupt_wensday=14;
-const int interrupt_thursday=27;
-const int interrupt_friday=26;
+const int interrupt_monday=2;
+const int interrupt_tuesday=4;
+const int interrupt_wensday=16;
+const int interrupt_thursday=13;
+const int interrupt_friday=12;
 
 extern const int pin_arrary[5]={interrupt_monday,interrupt_tuesday,interrupt_wensday,interrupt_thursday,interrupt_friday};
 
@@ -33,24 +34,50 @@ extern const uint16_t PROGMEM *wensday[2];
 extern const uint16_t PROGMEM *thursday[3];
 extern const uint16_t PROGMEM friday[];
 
+//状态机处理
+extern volatile uint state_mach=0;
+//状态机执行函数：
+extern void output_monday();
+extern void output_tuesday();
+extern void output_wensday();
+extern void output_thursday();
+extern void output_friday();
+
 void setup()
 { 
 for(int i=0;i<5;i++){
   pinMode(pin_arrary[i],INPUT);
-    attachInterrupt(pin_arrary[i],function_class[i],LOW);
+    attachInterrupt(pin_arrary[i],function_class[i],FALLING);
   }
 
  tft.begin();
 tft.setRotation(1);
  tft.fillScreen(TFT_BLACK);
  showstarting();
- for(int i=30;i>0;i--)
-robot_face();
-// tft.fillScreen(TFT_BLACK);
-// output_allclass();
+ delay(500);
  tft.fillScreen(TFT_BLACK);
 }
 void loop()
 {
+  switch(state_mach){
+    case 1:
+    output_monday();
+    state_mach=0;
+    break;
+    case 2:
+    output_tuesday();
+    state_mach=0;
+    break;
+    case 3:
+    output_wensday();
+    state_mach=0;
+    break;
+    case 4:
+    state_mach=0;
+    case 5:
+    state_mach=0;
+    break;
+    default:
     robot_face();
-}
+  }
+  }
