3a27f7aa81a3d5b3394290cf6cea060e9854e7a7
xfp23 <120090946+xfp23@users.noreply.github.com>
2023-11-26 14:28:59 +0800

    Update interupt.ino

--- interupt.ino
+++ interupt.ino
@@ -8,32 +8,58 @@ extern const uint16_t PROGMEM *thursday[3];
 extern const uint16_t PROGMEM friday[];
 extern void output_allclass();
 
+extern volatile uint state_mach;
+
 //外部中断函数
 extern void Display_monday(){
-for(int i=0;i<4;i++){
+    state_mach=1;
+ }
+ 
+extern void Display_tuesday()
+{
+   state_mach=2;
+}
+extern void Display_wensday(){
+  state_mach=3;
+  }
+extern void Display_thursday()
+{
+    state_mach=4;
+}
+extern void Display_friday()
+  {
+    state_mach=5;
+  }
+ extern void output_monday()
+  {
+    for(int i=0;i<4;i++){
     tft.pushImage(0,0,WIDTH,HIGHG,monday[i]);//宽度，高度
     delay(2000);
- }
- }
-extern void Display_tuesday()
- {
-   tft.pushImage(0,0,WIDTH,HIGHG,tuesday);
+    //tft.fillScreen(TFT_BLUE);
+    }}
+ extern void output_tuesday()
+    {
+      tft.pushImage(0,0,WIDTH,HIGHG,tuesday);
    delay(2000);
-  }
-extern void Display_wensday(){
-  for(int i=0;i<2;i++){
+   //tft.fillScreen(TFT_BLUE);
+     }
+extern void output_wensday()
+     {
+      for(int i=0;i<2;i++){
   tft.pushImage(0,0,WIDTH,HIGHG,wensday[i]);
   delay(2000);
-  }}
-extern void Display_thursday()
-  {
+  //tft.fillScreen(TFT_BLUE);
+     }}
+extern void output_thursday()
+     {
       for(int i=0;i<3;i++){
     tft.pushImage(0,0,WIDTH,HIGHG,thursday[i]);
     delay(2000);
-    }
-  }
-extern void Display_friday()
-  {
-    tft.pushImage(0,0,WIDTH,HIGHG,friday);
+   // tft.fillScreen(TFT_BLUE);
+     }}
+extern void output_friday()
+     {
+      tft.pushImage(0,0,WIDTH,HIGHG,friday);
     delay(2000);
-  }
+   // tft.fillScreen(TFT_BLUE);
+      }
